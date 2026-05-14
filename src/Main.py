3bb8d1d07161518358e621b83ca3e8b36293ba97
Rohit A <u3321763@uni.canberra.edu.au>
2026-05-15 00:28:08 +1000

    added more entries and labels

--- src/Main.py
+++ src/Main.py
@@ -19,6 +19,21 @@ class RootApp:
 
         # labels
         self.text_box_label = tk.Label(self.root, text="log")
+
+        self.loan_amount_label = tk.Label(self.root, text="Loan Amount")
+        self.annual_interest_rate_label = tk.Label(self.root, text="Annual Interest Rate")
+        self.loan_term_label = tk.Label(self.root, text="Loan Term")
+        self.monthly_income_label = tk.Label(self.root, text="Monthly Income")
+        self.monthly_expenses_label = tk.Label(self.root, text="Monthly Expenses")
+
+        #Entry
+        self.loan_amount_entry = tk.Entry(self.root, width=10)
+        self.annual_interest_rate_entry = tk.Entry(self.root, width=10)
+        self.loan_term_label_entry = tk.Entry(self.root, width=10)
+        self.monthly_income_entry = tk.Entry(self.root, width=10)
+        self.monthly_expenses_entry = tk.Entry(self.root, width=10)
+
+        # text box
         self.text_box = tk.Text(self.root, height=30, width=100)
 
         # text box settings
@@ -27,14 +42,31 @@ class RootApp:
         #grid settings
         self.grid_setting = {
             "padx": 10, 
+            "pady": 5,
             "sticky": "w"
             }
 
         #grid
-        self.text_box_label.grid(row=1, column=0, **self.grid_setting)
-        self.text_box.grid(row=2, column=0, **self.grid_setting)
-        self.button.grid(row=3, column=0, pady=5, **self.grid_setting)
+        self.text_box_label.grid(row=0, column=0, **self.grid_setting)
+        self.text_box.grid(row=2, column=0, columnspan=2, **self.grid_setting)
+        self.button.grid(row=3, column=0, **self.grid_setting)
+            
+            #labels
+        self.loan_amount_label.grid(row=4, column=0, **self.grid_setting)
+        self.annual_interest_rate_label.grid(row=5, column=0, **self.grid_setting)
+        self.loan_term_label.grid(row=6, column=0, **self.grid_setting)
+        self.monthly_income_label.grid(row=7, column=0, **self.grid_setting)
+        self.monthly_expenses_label.grid(row=8, column=0, **self.grid_setting)
 
+            #Entry
+        self.loan_amount_entry.grid(row=4, column=1, **self.grid_setting)
+        self.annual_interest_rate_entry.grid(row=5, column=1, **self.grid_setting)
+        self.loan_term_label_entry.grid(row=6, column=1, **self.grid_setting)
+        self.monthly_income_entry.grid(row=7, column=1, **self.grid_setting)
+        self.monthly_expenses_entry.grid(row=8, column=1, **self.grid_setting)
+
+    
+    
     def write_msg(self, message):
         self.message = message
         self.text_box.config(state="normal")
